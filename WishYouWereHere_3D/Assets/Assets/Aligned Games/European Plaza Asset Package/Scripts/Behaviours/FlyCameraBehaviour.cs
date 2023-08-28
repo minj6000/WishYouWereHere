ba0fd0ac11f24258e1c1d3cdd006e44267dbf600
bbangwon <bbangwon.chung@gmail.com>
2023-08-28 23:18:13 +0900

    02EP 에셋 추가

---
+++ WishYouWereHere_3D/Assets/Assets/Aligned Games/European Plaza Asset Package/Scripts/Behaviours/FlyCameraBehaviour.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyCameraBehaviour : MonoBehaviour
+
+{
+
+    public float CameraSensitivity = 90;
+    public float ClimbSpeed = 4;
+    public float NormalMoveSpeed = 10;
+    public float SlowMoveFactor = 0.25f;
+    public float FastMoveFactor = 3;
+
+    private float RotationX = 0.0f;
+    private float RotationY = 0.0f;
+
+    public bool CameraActive;
+
+    public void Start()
+
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    public void Update()
+
+    {
+
+        if (Input.anyKey)
+
+        {
+
+            CameraActive = true;
+
+        }
+
+        if (CameraActive)
+
+        {
+
+            RotationX += Input.GetAxis("Mouse X") * CameraSensitivity * Time.deltaTime;
+            RotationY += Input.GetAxis("Mouse Y") * CameraSensitivity * Time.deltaTime;
+            RotationY = Mathf.Clamp(RotationY, -90, 90);
+
+            transform.localRotation = Quaternion.AngleAxis(RotationX, Vector3.up);
+            transform.localRotation *= Quaternion.AngleAxis(RotationY, Vector3.left);
+
+            if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+
+            {
+
+                transform.position += transform.forward * (NormalMoveSpeed * FastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += transform.right * (NormalMoveSpeed * FastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+            }
+
+            else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+
+            {
+
+                transform.position += transform.forward * (NormalMoveSpeed * SlowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += transform.right * (NormalMoveSpeed * SlowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+            }
+
+            else
+
+            {
+
+                transform.position += transform.forward * NormalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += transform.right * NormalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+            }
+
+            if (Input.GetKey(KeyCode.Q))
+
+            {
+
+                transform.position += transform.up * ClimbSpeed * Time.deltaTime;
+
+            }
+
+            if (Input.GetKey(KeyCode.E))
+
+            {
+
+                transform.position -= transform.up * ClimbSpeed * Time.deltaTime;
+
+            }
+
+        }
+
+    }
+
+}
