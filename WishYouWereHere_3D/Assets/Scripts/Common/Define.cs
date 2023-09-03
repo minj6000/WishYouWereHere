7dc01ff6ff827dbe8115a2361a2e573528448a0e
bbangwon <bbangwon.chung@gmail.com>
2023-09-03 14:38:51 +0900

    라이트닝 맵 추가, Listen Scene에서 에피소드 씬으로 이동 기능 추가

---
+++ WishYouWereHere_3D/Assets/Scripts/Common/Define.cs
@@ -0,0 +1,17 @@
+namespace WishYouWereHere3D.Common
+{
+    public class Define
+    {
+        public const string OSC_ADDRESS = "/WishYouWereHere";
+        
+        public const string SCENE_NAME_EP1 = "01EP";
+        public const string SCENE_NAME_EP2 = "02EP";
+        public const string SCENE_NAME_EP3 = "03EP";
+        public const string SCENE_NAME_EP4 = "04EP";
+
+
+        
+
+
+    }
+}
